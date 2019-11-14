c595ab49b593e20a72ead766d309d9363f0eddf9
uuxia <263996097@qq.com>
2019-11-14 14:56:54 +0800

    xiaxiaoli_2005
    xiaxiaoli_2005

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.29.1
 export FRPS_INIT="https://raw.githubusercontent.com/xxl6097/onekey-install-shell/master/frps/frps.init"
-export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export aliyun_download_url="https://code.aliyun.com/xiaxiaoli_2005/frp/raw/master/zip"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
