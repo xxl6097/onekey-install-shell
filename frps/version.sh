5d78e7d0c5be7354d243b1b3ff3205cfe76f9893
uuxia <263996097@qq.com>
2020-02-16 13:21:28 +0800

    呜呜呜呜
         吾问无为谓

--- frps/version.sh
+++ frps/version.sh
@@ -3,3 +3,6 @@ export FRPS_VER=0.29.1
 export FRPS_INIT="https://raw.githubusercontent.com/xxl6097/onekey-install-shell/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/xiaxiaoli_2005/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
+
+
+
